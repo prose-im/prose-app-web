3e3c9b12aaa9b9f51aa70c708574eb5207c2f2e0
Valerian Saliou <valerian@valeriansaliou.name>
2023-01-05 17:14:22 +0100

    feat: run eslint on builds

--- vite.config.ts
+++ vite.config.ts
@@ -12,11 +12,12 @@ import { defineConfig } from "vite";
 
 import vue from "@vitejs/plugin-vue";
 import vitePugPlugin from "vite-plugin-pug-transformer";
+import viteESLintPlugin from 'vite-plugin-eslint';
 
 /**************************************************************************
  * EXPORTS
  * ************************************************************************* */
 
 export default defineConfig({
-  plugins: [vue(), vitePugPlugin()]
+  plugins: [vue(), vitePugPlugin(), viteESLintPlugin()]
 });
