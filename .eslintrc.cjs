20fd276974e2184d2a51bcb8d172a1ba00a52eae
Valerian Saliou <valerian@valeriansaliou.name>
2023-01-05 17:34:20 +0100

    fix: parsers as object

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,7 +9,7 @@ module.exports = {
   parser: "vue-eslint-parser",
 
   parserOptions: {
-    parser: "@typescript-eslint/parser",
+    parser: ["@typescript-eslint/parser"],
     sourceType: "module"
   },
 
