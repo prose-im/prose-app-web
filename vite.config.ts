9ed5541504f0ead4b8677c19956ddad3a82321b6
Valerian Saliou <valerian@valeriansaliou.name>
2023-01-05 17:16:39 +0100

    fix: do not use any vite eslint plugin anymore, this should be an editor module

--- vite.config.ts
+++ vite.config.ts
@@ -12,12 +12,11 @@ import { defineConfig } from "vite";
 
 import vue from "@vitejs/plugin-vue";
 import vitePugPlugin from "vite-plugin-pug-transformer";
-import viteESLintPlugin from 'vite-plugin-eslint';
 
 /**************************************************************************
  * EXPORTS
  * ************************************************************************* */
 
 export default defineConfig({
-  plugins: [vue(), vitePugPlugin(), viteESLintPlugin()]
+  plugins: [vue(), vitePugPlugin()]
 });
