85fb9c16ca879b2ac1312b635f887079db43bcb4
Valerian Saliou <valerian@valeriansaliou.name>
2023-01-05 14:48:06 +0100

    chore: bootstrap base project structure

---
+++ src/bootstrap/components.ts
@@ -0,0 +1,58 @@
+/*
+ * This file is part of prose-app-web
+ *
+ * Copyright 2023, Prose Foundation
+ */
+
+/**************************************************************************
+ * IMPORTS
+ ***************************************************************************/
+
+// NPM
+import upperFirst from "lodash.upperfirst";
+import camelCase from "lodash.camelcase";
+
+/**************************************************************************
+ * COMPONENTS
+ ***************************************************************************/
+
+class Components {
+  /**
+   * Initializer
+   * @param  {object} app
+   * @return {undefined}
+   */
+  init(app) {
+    this.registerGlobally(
+      app, import.meta.globEager("../components/base/*.vue")
+    );
+  }
+
+  /**
+   * Registers component globally
+   * @param  {object}   app
+   * @param  {function} componentFiles
+   * @return {undefined}
+   */
+  registerGlobally(app, componentFiles) {
+    Object.entries(componentFiles).forEach(
+      ([componentPath, componentModule]) => {
+        // Get PascalCase name of component
+        const componentName = upperFirst(
+          camelCase(
+            componentPath.split("/").pop().replace(/^(.+)\.vue$/, "$1")
+          )
+        );
+
+        // Register component globally
+        app.component(componentName, componentModule.default);
+      }
+    );
+  }
+}
+
+/**************************************************************************
+ * EXPORTS
+ ***************************************************************************/
+
+export default new Components();
---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+/*
+ * This file is part of prose-app-web
+ *
+ * Copyright 2023, Prose Foundation
+ */
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+})
