67479357b6bac87308dd7edac19262481f59825e
Valerian Saliou <valerian@valeriansaliou.name>
2023-01-15 18:58:40 +0100

    fix: inbox skeleton structure

--- src/views/app/inbox/AppInboxBase.vue
+++ src/views/app/inbox/AppInboxBase.vue
@@ -10,16 +10,17 @@
 
 <template lang="pug">
 .v-app-inbox-base
+  .v-app-inbox-base__topbar
+
   .v-app-inbox-base__content
-    .v-app-inbox-base__topbar
+    .v-app-inbox-base__messages
+      inbox-messaging(
+        class="v-app-inbox-base__timeline"
+      )
 
-    inbox-messaging(
-      class="v-app-inbox-base__timeline"
-    )
+      .v-app-inbox-base__form
 
-    .v-app-inbox-base__form
-
-  .v-app-inbox-base__userinfo
+    .v-app-inbox-base__userinfo
 </template>
 
 <!-- **********************************************************************
@@ -48,18 +49,7 @@ $c: ".v-app-inbox-base";
   height: 100%;
   width: 100%;
   display: flex;
-
-  #{$c}__content {
-    display: flex;
-    flex: 1;
-    flex-direction: column;
-  }
-
-  #{$c}__userinfo {
-    border-left: 1px solid $color-base-grey-normal;
-    width: 280px;
-    flex: 0 0 auto;
-  }
+  flex-direction: column;
 
   #{$c}__topbar {
     border-bottom: 1px solid $color-base-grey-normal;
@@ -67,16 +57,33 @@ $c: ".v-app-inbox-base";
     flex: 0 0 auto;
   }
 
-  #{$c}__timeline {
-    width: 100%;
-    height: 100%;
+  #{$c}__content {
     flex: 1;
-  }
+    display: flex;
 
-  #{$c}__form {
-    border-top: 1px solid $color-base-grey-normal;
-    height: 90px;
-    flex: 0 0 auto;
+    #{$c}__messages {
+      display: flex;
+      flex: 1;
+      flex-direction: column;
+
+      #{$c}__timeline {
+        width: 100%;
+        height: 100%;
+        flex: 1;
+      }
+
+      #{$c}__form {
+        border-top: 1px solid $color-base-grey-normal;
+        height: 90px;
+        flex: 0 0 auto;
+      }
+    }
+
+    #{$c}__userinfo {
+      border-left: 1px solid $color-base-grey-normal;
+      width: 280px;
+      flex: 0 0 auto;
+    }
   }
 }
 </style>
