6e2b1d5e01668a50ea0574f800285529ff379f4b
Valerian Saliou <valerian@valeriansaliou.name>
2023-02-04 16:32:55 -0500

    feat: base-count component

---
+++ src/components/base/BaseCount.vue
@@ -0,0 +1,54 @@
+<!--
+ * This file is part of prose-app-web
+ *
+ * Copyright 2023, Prose Foundation
+ -->
+
+<!-- **********************************************************************
+     TEMPLATE
+     ********************************************************************** -->
+
+<template lang="pug">
+.c-base-count.u-medium
+  | {{ count }}
+</template>
+
+<!-- **********************************************************************
+     SCRIPT
+     ********************************************************************** -->
+
+<script lang="ts">
+export default {
+  name: "BaseCount",
+
+  props: {
+    count: {
+      type: Number,
+      default: 0
+    }
+  }
+};
+</script>
+
+<!-- **********************************************************************
+     STYLE
+     ********************************************************************** -->
+
+<style lang="scss">
+$c: ".c-base-count";
+
+// VARIABLES
+$count-size-base: 20px;
+$count-padding-sides: 5px;
+
+.c-base-count {
+  background-color: $color-base-blue-normal;
+  color: $color-white;
+  font-size: 12px;
+  text-align: center;
+  line-height: $count-size-base;
+  padding-inline: $count-padding-sides;
+  min-width: ($count-size-base - (2 * $count-padding-sides));
+  border-radius: 4px;
+}
+</style>
