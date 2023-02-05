cc06689db7f363f9d160d4a6556aba02a0eabc3d
Valerian Saliou <valerian@valeriansaliou.name>
2023-02-04 20:08:20 -0500

    feat: finalize scroll management in sidebar

--- src/views/app/AppBase.vue
+++ src/views/app/AppBase.vue
@@ -50,6 +50,8 @@ $c: ".v-app-base";
 
 // VARIABLES
 $sidebar-items-padding-sides: 20px;
+$sidebar-main-padding-block: 20px;
+$sidebar-context-height: $size-inbox-form-height;
 
 .v-app-base {
   height: 100%;
@@ -65,8 +67,7 @@ $sidebar-items-padding-sides: 20px;
     border-inline-end: 1px solid $color-border-secondary;
     width: 300px;
     flex: 0 0 auto;
-    display: flex;
-    flex-direction: column;
+    position: relative;
 
     #{$c}__sidebar-main,
     #{$c}__sidebar-context {
@@ -74,23 +75,15 @@ $sidebar-items-padding-sides: 20px;
     }
 
     #{$c}__sidebar-main {
-      padding-block: 20px;
-      flex: 1;
-      position: relative;
-
-      &:after {
-        content: "";
-        background-image: linear-gradient(
-          180deg,
-          rgba($color-black, 0) 0%,
-          rgba($color-black, 0.01) 100%
-        );
-        height: 6px;
-        position: absolute;
-        inset-inline: 0;
-        inset-block-end: 0;
-        pointer-events: none;
-      }
+      padding-block-start: $sidebar-main-padding-block;
+      padding-block-end: (
+        $sidebar-main-padding-block + $sidebar-context-height
+      );
+      overflow-x: hidden;
+      overflow-y: auto;
+      position: absolute;
+      inset: 0;
+      z-index: 0;
 
       #{$c}__sidebar-main-list {
         margin-inline: (-1 * $sidebar-items-padding-sides);
@@ -102,9 +95,28 @@ $sidebar-items-padding-sides: 20px;
     }
 
     #{$c}__sidebar-context {
+      background-color: rgba($color-base-purple-ultra-light, 0.9);
       border-block-start: 1px solid $color-border-secondary;
-      height: $size-inbox-form-height;
-      flex: 0 0 auto;
+      height: $sidebar-context-height;
+      position: absolute;
+      inset-inline: 0;
+      inset-block-end: 0;
+      z-index: 1;
+      backdrop-filter: blur(9px);
+
+      &:before {
+        content: "";
+        background-image: linear-gradient(
+          180deg,
+          rgba($color-black, 0) 0%,
+          rgba($color-black, 0.01) 100%
+        );
+        height: 6px;
+        position: absolute;
+        inset-inline: 0;
+        inset-block-end: calc(100% + 1px);
+        pointer-events: none;
+      }
     }
   }
 
