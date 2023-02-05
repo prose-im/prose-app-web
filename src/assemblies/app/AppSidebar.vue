860853949c3a2b283b7f6e0799df193d2587f360
Valerian Saliou <valerian@valeriansaliou.name>
2023-02-04 20:47:03 -0500

    feat: implement sidebar-header

--- src/assemblies/app/AppSidebar.vue
+++ src/assemblies/app/AppSidebar.vue
@@ -10,6 +10,10 @@
 
 <template lang="pug">
 .a-app-sidebar
+  sidebar-header(
+    class="a-app-sidebar__actions"
+  )
+
   sidebar-main(
     class="a-app-sidebar__main"
     disclosure-list-class="a-app-sidebar__main-list"
@@ -27,13 +31,14 @@
 
 <script lang="ts">
 // PROJECT: COMPONENTS
+import SidebarHeader from "@/components/sidebar/SidebarHeader.vue";
 import SidebarMain from "@/components/sidebar/SidebarMain.vue";
 import SidebarContext from "@/components/sidebar/SidebarContext.vue";
 
 export default {
   name: "AppSidebar",
 
-  components: { SidebarMain, SidebarContext }
+  components: { SidebarHeader, SidebarMain, SidebarContext }
 };
 </script>
 
@@ -46,7 +51,10 @@ $c: ".a-app-sidebar";
 
 // VARIABLES
 $sidebar-items-padding-sides: 20px;
-$sidebar-main-padding-block: 20px;
+$sidebar-main-padding-block: 16px;
+$sidebar-header-border-width: 1px;
+$sidebar-header-height: $size-inbox-topbar-height;
+$sidebar-context-border-width: 1px;
 $sidebar-context-height: $size-inbox-form-height;
 
 .a-app-sidebar {
@@ -57,14 +65,35 @@ $sidebar-context-height: $size-inbox-form-height;
     background-color: $color-base-purple-ultra-light;
   }
 
+  #{$c}__actions,
   #{$c}__main,
   #{$c}__context {
     padding-inline: $sidebar-items-padding-sides;
   }
 
+  #{$c}__actions,
+  #{$c}__context {
+    background-color: rgba($color-base-purple-ultra-light, 0.9);
+    position: absolute;
+    inset-inline: 0;
+    backdrop-filter: blur(9px);
+  }
+
+  #{$c}__actions {
+    height: $sidebar-header-height;
+    inset-block-start: 0;
+    z-index: 1;
+  }
+
   #{$c}__main {
-    padding-block-start: $sidebar-main-padding-block;
-    padding-block-end: ($sidebar-main-padding-block + $sidebar-context-height);
+    padding-block-start: (
+      $sidebar-main-padding-block + $sidebar-header-height +
+        $sidebar-header-border-width
+    );
+    padding-block-end: (
+      $sidebar-main-padding-block + $sidebar-context-height +
+        $sidebar-context-border-width
+    );
     overflow-x: hidden;
     overflow-y: auto;
     position: absolute;
@@ -81,14 +110,11 @@ $sidebar-context-height: $size-inbox-form-height;
   }
 
   #{$c}__context {
-    background-color: rgba($color-base-purple-ultra-light, 0.9);
-    border-block-start: 1px solid $color-border-secondary;
+    border-block-start: $sidebar-context-border-width solid
+      $color-border-secondary;
     height: $sidebar-context-height;
-    position: absolute;
-    inset-inline: 0;
     inset-block-end: 0;
-    z-index: 1;
-    backdrop-filter: blur(9px);
+    z-index: 2;
 
     &:before {
       content: "";
---
+++ src/components/sidebar/SidebarHeader.vue
@@ -0,0 +1,55 @@
+<!--
+ * This file is part of prose-app-web
+ *
+ * Copyright 2023, Prose Foundation
+ -->
+
+<!-- **********************************************************************
+     TEMPLATE
+     ********************************************************************** -->
+
+<template lang="pug">
+.c-sidebar-header
+  layout-actions(
+    class="c-sidebar-header__actions"
+  )
+    base-action(
+      class="c-sidebar-header__action"
+      icon="phone.bubble.left"
+      size="18px"
+    )
+
+    base-action(
+      class="c-sidebar-header__action"
+      icon="square.and.pencil"
+      size="18px"
+    )
+</template>
+
+<!-- **********************************************************************
+     SCRIPT
+     ********************************************************************** -->
+
+<script lang="ts">
+export default {
+  name: "SidebarHeader"
+};
+</script>
+
+<!-- **********************************************************************
+     STYLE
+     ********************************************************************** -->
+
+<style lang="scss">
+$c: ".c-sidebar-header";
+
+.c-sidebar-header {
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+
+  #{$c}__actions {
+    margin-inline-end: (-1 * ($size-base-action-padding-sides - 1px));
+  }
+}
+</style>
