ebdc06559a1c416e1c651c43a3ae8fd9eb540f01
Valerian Saliou <valerian@valeriansaliou.name>
2023-02-06 11:12:21 -0500

    feat: add server logo component

---
+++ src/components/base/BaseServerLogo.vue
@@ -0,0 +1,94 @@
+<!--
+ * This file is part of prose-app-web
+ *
+ * Copyright 2023, Prose Foundation
+ -->
+
+<!-- **********************************************************************
+     TEMPLATE
+     ********************************************************************** -->
+
+<template lang="pug">
+div(
+  :class=`[
+    "c-base-server-logo",
+    "c-base-server-logo--shadow-" + shadow
+  ]`
+  :style=`{
+    backgroundImage: backgroundImage,
+    height: size,
+    width: size
+  }`
+)
+</template>
+
+<!-- **********************************************************************
+     SCRIPT
+     ********************************************************************** -->
+
+<script lang="ts">
+export default {
+  name: "BaseServerLogo",
+
+  props: {
+    domain: {
+      type: String,
+      required: true
+    },
+
+    size: {
+      type: String,
+      default: "100px"
+    },
+
+    shadow: {
+      type: String,
+      default: "normal",
+
+      validator(x: string) {
+        return ["none", "normal"].includes(x);
+      }
+    }
+  },
+
+  computed: {
+    logoImageUrl() {
+      // TODO: acquire from cache using provided domain, this is only a \
+      //   temporary fixture
+      const handle = this.domain.replaceAll(".", "-");
+
+      return [
+        "/src/assets/images/components/base/BaseServerLogo",
+        `logo-${handle}.png`
+      ].join("/");
+    },
+
+    backgroundImage() {
+      return `url("${this.logoImageUrl}")`;
+    }
+  }
+};
+</script>
+
+<!-- **********************************************************************
+     STYLE
+     ********************************************************************** -->
+
+<style lang="scss">
+$c: ".c-base-server-logo";
+
+.c-base-server-logo {
+  background-color: $color-base-grey-light;
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  display: inline-block;
+  border-radius: 100%;
+
+  // --> SHADOWS <--
+
+  &--shadow-normal {
+    box-shadow: 0 2px 4px 0 rgba($color-black, 0.08);
+  }
+}
+</style>
