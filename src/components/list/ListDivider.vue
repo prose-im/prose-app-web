4db7c60353284fc31693ae464ebc4783fb2381a0
Valerian Saliou <valerian@valeriansaliou.name>
2023-02-08 12:27:29 -0500

    feat: list-divider component

---
+++ src/components/list/ListDivider.vue
@@ -0,0 +1,38 @@
+<!--
+ * This file is part of prose-app-web
+ *
+ * Copyright 2023, Prose Foundation
+ -->
+
+<!-- **********************************************************************
+     TEMPLATE
+     ********************************************************************** -->
+
+<template lang="pug">
+span.c-list-divider
+</template>
+
+<!-- **********************************************************************
+     SCRIPT
+     ********************************************************************** -->
+
+<script lang="ts">
+export default {
+  name: "ListDivider"
+};
+</script>
+
+<!-- **********************************************************************
+     STYLE
+     ********************************************************************** -->
+
+<style lang="scss">
+$c: ".c-list-divider";
+
+.c-list-divider {
+  background-color: $color-base-grey-light;
+  height: 1px;
+  margin-block: 7px;
+  display: block;
+}
+</style>
