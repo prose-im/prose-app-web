d6ce58aa6515a585c41d303152be37cc26e80a73
Valerian Saliou <valerian@valeriansaliou.name>
2023-02-09 21:31:20 -0500

    chore: commonize base-popover variables

--- src/components/base/BasePopover.vue
+++ src/components/base/BasePopover.vue
@@ -32,11 +32,11 @@ $c: ".c-base-popover";
 
 .c-base-popover {
   background-color: $color-white;
-  border: 1px solid darken($color-base-grey-light, 2%);
+  border: $size-base-popover-border-width solid
+    darken($color-base-grey-light, 2%);
   min-width: 180px;
   max-width: 360px;
   padding-block: 7px;
-  overflow: hidden;
   border-radius: 5px;
   box-shadow: 0 2px 6px 0 rgba($color-black, 0.025);
 }
