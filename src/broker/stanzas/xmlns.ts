b2026d8a9c5500f0c63ad48a8bcac7d3d33809c3
Valerian Saliou <valerian@valeriansaliou.name>
2023-02-25 13:21:09 +0100

    feat: define stanza xmlns

---
+++ src/broker/stanzas/xmlns.ts
@@ -0,0 +1,16 @@
+/*
+ * This file is part of prose-app-web
+ *
+ * Copyright 2023, Prose Foundation
+ */
+
+/**************************************************************************
+ * CONSTANTS
+ * ************************************************************************* */
+
+export const NS_FASTEN = "urn:xmpp:fasten:0";
+export const NS_FALLBACK = "urn:xmpp:fallback:0";
+export const NS_HINTS = "urn:xmpp:hints";
+
+export const NS_MESSAGE_CORRECT = "urn:xmpp:message-correct:0";
+export const NS_MESSAGE_RETRACT = "urn:xmpp:message-retract:0";
