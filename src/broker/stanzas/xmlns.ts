ba79652cb6a0d8a3024b19c6afec552d8e1bb900
Valerian Saliou <valerian@valeriansaliou.name>
2023-02-26 00:50:39 +0100

    feat: add more xmlns

--- src/broker/stanzas/xmlns.ts
+++ src/broker/stanzas/xmlns.ts
@@ -11,11 +11,14 @@
 export const NS_VCARD4 = "urn:ietf:params:xml:ns:vcard-4.0";
 
 export const NS_ROSTER = "jabber:iq:roster";
+export const NS_VERSION = "jabber:iq:version";
+export const NS_LAST = "jabber:iq:last";
 export const NS_DATA = "jabber:x:data";
 
 export const NS_FASTEN = "urn:xmpp:fasten:0";
 export const NS_FALLBACK = "urn:xmpp:fallback:0";
 export const NS_HINTS = "urn:xmpp:hints";
+export const NS_TIME = "urn:xmpp:time";
 export const NS_PING = "urn:xmpp:ping";
 export const NS_REACTIONS = "urn:xmpp:reactions:0";
 export const NS_CARBONS = "urn:xmpp:carbons:2";
@@ -26,3 +29,5 @@ export const NS_MESSAGE_RETRACT = "urn:xmpp:message-retract:0";
 
 export const NS_CHAT_STATES = "http://jabber.org/protocol/chatstates";
 export const NS_RSM = "http://jabber.org/protocol/rsm";
+export const NS_CAPS = "http://jabber.org/protocol/caps";
+export const NS_DISCO_INFO = "http://jabber.org/protocol/disco#info";
