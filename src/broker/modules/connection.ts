efbe465cb872a2b95f20499e6148abb9534daafc
Valerian Saliou <valerian@valeriansaliou.name>
2023-02-25 15:59:22 +0100

    feat: implement XEP-0199

--- src/broker/modules/connection.ts
+++ src/broker/modules/connection.ts
@@ -8,8 +8,13 @@
  * IMPORTS
  * ************************************************************************* */
 
+// NPM
+import { $pres } from "strophe.js";
+
 // PROJECT: BROKER
 import BrokerModule from "@/broker/modules";
+import { IQType } from "@/broker/stanzas/iq";
+import { NS_PING } from "@/broker/stanzas/xmlns";
 
 /**************************************************************************
  * CLASS
@@ -17,11 +22,15 @@ import BrokerModule from "@/broker/modules";
 
 class BrokerModuleConnection extends BrokerModule {
   sendInitialPresence(): void {
-    // TODO
+    // XMPP: Instant Messaging and Presence
+    // https://xmpp.org/rfcs/rfc6121.html
+    this.__client.emit($pres());
   }
 
   sendPing(): void {
-    // TODO
+    // XEP-0199: XMPP Ping
+    // https://xmpp.org/extensions/xep-0199.html
+    this.__client.emit($iq({ type: IQType.Get }).c("ping", { xmlns: NS_PING }));
   }
 }
 
