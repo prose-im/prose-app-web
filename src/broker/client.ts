0549737541a900bcdab1e3bc44c15edbd982c02f
Valerian Saliou <valerian@valeriansaliou.name>
2023-02-26 12:09:48 +0100

    fix: reuse handlers

--- src/broker/client.ts
+++ src/broker/client.ts
@@ -166,27 +166,6 @@ class BrokerClient {
     logger.debug("(out)", data);
   }
 
-  private __onReceivePresence(stanza: Element): void {
-    logger.log("(presence)", stanza);
-
-    // Pass to presence ingestor
-    this.__ingestors.presence(stanza);
-  }
-
-  private __onReceiveMessage(stanza: Element): void {
-    logger.log("(message)", stanza);
-
-    // Pass to message ingestor
-    this.__ingestors.message(stanza);
-  }
-
-  private __onReceiveIQ(stanza: Element): void {
-    logger.log("(iq)", stanza);
-
-    // Pass to IQ ingestor
-    this.__ingestors.iq(stanza);
-  }
-
   private __raiseConnectLifecycle(error?: Error): void {
     if (this.__connectLifecycle) {
       if (error) {
@@ -209,17 +188,24 @@ class BrokerClient {
   private __bindReceivers(): void {
     // Not already bound? Bind all receivers
     if (this.__boundReceivers.length === 0) {
-      const handlers = {
-        presence: this.__onReceivePresence,
-        message: this.__onReceiveMessage,
-        iq: this.__onReceiveIQ
-      };
+      ["presence", "message", "iq"].forEach(handlerName => {
+        // Generate handler function
+        const handlerFn = (stanza: Element) => {
+          logger.log(`(${handlerName})`, stanza);
 
-      for (const [handlerName, handlerFn] of Object.entries(handlers)) {
+          // Pass to specific ingestor
+          this.__ingestors[handlerName](stanza);
+
+          // Important: keep handler alive (otherwise it will get torn out \
+          //   after it gets fired once)
+          return true;
+        };
+
+        // Append handler
         this.__boundReceivers.push(
-          this.__connection.addHandler(handlerFn.bind(this), null, handlerName)
+          this.__connection.addHandler(handlerFn, null, handlerName)
         );
-      }
+      });
     }
   }
 
