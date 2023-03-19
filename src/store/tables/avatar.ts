e0351c2aa16cb97a26785c09f886b671c110f68f
Valerian Saliou <valerian@valeriansaliou.name>
2023-03-19 11:08:26 +0100

    feat: define avatar store

--- src/store/tables/avatar.ts
+++ src/store/tables/avatar.ts
@@ -9,7 +9,35 @@
  * ************************************************************************* */
 
 // NPM
+import { JID } from "@xmpp/jid";
 import { defineStore } from "pinia";
+
+/**************************************************************************
+ * TYPES
+ * ************************************************************************* */
+
+type AvatarEntryData = string;
+
+type AvatarEntryMetadata = {
+  id: string;
+  type: string;
+  bytes: number;
+  height: number;
+  width: number;
+};
+
+/**************************************************************************
+ * INTERFACES
+ * ************************************************************************* */
+
+interface Avatar {
+  [jid: string]: AvatarEntry;
+}
+
+interface AvatarEntry {
+  metadata: AvatarEntryMetadata;
+  data: AvatarEntryData;
+}
 
 /**************************************************************************
  * TABLE
@@ -18,12 +46,14 @@ import { defineStore } from "pinia";
 const $avatar = defineStore("avatar", {
   persist: true,
 
-  state: () => {
-    return {
-      images: {
-        // TODO: [jid]: { data, metadata }
-      }
-    };
+  state: (): Avatar => {
+    return {};
+  },
+
+  getters: {
+    getAvatar: (state: Avatar) => {
+      return (jid: JID) => state[jid.toString()] || null;
+    }
   }
 });
 
