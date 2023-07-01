fd7b14021884d3e86cbb6c7e84e9a7813b4cf959
Valerian Saliou <valerian@valeriansaliou.name>
2023-07-01 13:53:25 +0200

    feat: add base-popup skeleton component

---
+++ src/components/base/BasePopup.vue
@@ -0,0 +1,97 @@
+<!--
+ * This file is part of prose-app-web
+ *
+ * Copyright 2023, Prose Foundation
+ -->
+
+<!-- **********************************************************************
+     TEMPLATE
+     ********************************************************************** -->
+
+<template lang="pug">
+teleport(
+  to="#app"
+)
+  .c-base-popup(
+    v-hotkey="hotkeys"
+    v-bind="$attrs"
+  )
+    div(
+      :class=`[
+        "c-base-popup__popup",
+        "animate",
+        "animate--superfast",
+        "animate--fade-in-up-small",
+        {
+          [popupClass]: popupClass
+        }
+      ]`
+    )
+      slot
+</template>
+
+<!-- **********************************************************************
+     SCRIPT
+     ********************************************************************** -->
+
+<script lang="ts">
+export default {
+  name: "BasePopup",
+
+  props: {
+    popupClass: {
+      type: String,
+      default: null
+    }
+  },
+
+  emits: ["confirm", "close"],
+
+  computed: {
+    hotkeys() {
+      return {
+        enter: this.onHotkeyEnter,
+        esc: this.onHotkeyEscape
+      };
+    }
+  },
+
+  methods: {
+    // --> EVENT LISTENERS <--
+
+    onHotkeyEnter(): void {
+      // Confirm popup
+      this.$emit("confirm");
+    },
+
+    onHotkeyEscape(): void {
+      // Close popup
+      this.$emit("close");
+    }
+  }
+};
+</script>
+
+<!-- **********************************************************************
+     STYLE
+     ********************************************************************** -->
+
+<style lang="scss">
+$c: ".c-base-popup";
+
+.c-base-popup {
+  background-color: rgba($color-base-grey-dark, 0.35);
+  position: fixed;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  inset: 0;
+  z-index: 1000;
+
+  #{$c}__popup {
+    background-color: $color-white;
+    border-radius: 7px;
+    box-shadow: 0 4px 14px 0 rgba($color-black, 0.075);
+  }
+}
+</style>
