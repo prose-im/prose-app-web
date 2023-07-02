1a5dc31dcf587f6a908883383bc976339ea2df05
Valerian Saliou <valerian@valeriansaliou.name>
2023-07-02 10:41:46 +0200

    feat: lay out base structure for edit-profile popup

--- src/popups/sidebar/EditProfile.vue
+++ src/popups/sidebar/EditProfile.vue
@@ -10,11 +10,24 @@
 
 <template lang="pug">
 base-popup(
-  @close="$emit('close')"
-  @confirm="$emit('save')"
+  @close="onClose"
+  @confirm="onSave"
+  :keyboard-events="false"
   class="p-edit-profile"
   popup-class="p-edit-profile__popup"
 )
+  .p-edit-profile__navigate
+
+  .p-edit-profile__content
+    .p-edit-profile__form
+
+    base-popup-actions(
+      @confirm="onSave"
+      @cancel="onClose"
+      confirm-label="Save Profile"
+      cancel-label="Cancel"
+      class="p-edit-profile__actions"
+    )
 </template>
 
 <!-- **********************************************************************
@@ -25,7 +38,19 @@ base-popup(
 export default {
   name: "EditProfile",
 
-  emits: ["close", "save"]
+  emits: ["close", "save"],
+
+  methods: {
+    // --> EVENT LISTENERS <--
+
+    onSave(): void {
+      this.$emit("save");
+    },
+
+    onClose(): void {
+      this.$emit("close");
+    }
+  }
 };
 </script>
 
@@ -38,20 +63,60 @@ $c: ".p-edit-profile";
 
 // VARIABLES
 $popup-max-width: 720px;
-$popup-padding-sides: 16px;
+$popup-max-height: 600px;
+$popup-padding-sides: 22px;
+$popup-padding-top-bottom: $popup-padding-sides;
 
 $popup-width-full-margin-sides: 14px;
 $popup-width-full-breakpoint: (
-  $popup-max-width + (2 * $popup-padding-sides) + $popup-width-full-margin-sides
+  $popup-max-width + $popup-width-full-margin-sides
+);
+
+$popup-height-full-margin-top-bottom: $popup-width-full-margin-sides;
+$popup-height-full-breakpoint: (
+  $popup-max-height + $popup-height-full-margin-top-bottom
 );
 
 .p-edit-profile {
   #{$c}__popup {
     width: 100%;
+    height: 100%;
     max-width: $popup-max-width;
-    min-height: 420px;
+    max-height: $popup-max-height;
+    overflow: hidden;
+    display: flex;
+  }
+
+  #{$c}__navigate {
+    border-right: 1px solid $color-border-secondary;
     padding-inline: $popup-padding-sides;
-    padding-block: 20px;
+    padding-block: $popup-padding-top-bottom;
+    overflow: auto;
+    width: 220px;
+    flex: 0 0 auto;
+  }
+
+  #{$c}__content {
+    background-color: $color-background-secondary;
+    flex: 1;
+    display: flex;
+    flex-direction: column;
+
+    #{$c}__form,
+    #{$c}__actions {
+      padding-inline: $popup-padding-sides;
+    }
+
+    #{$c}__form {
+      padding-block-start: $popup-padding-top-bottom;
+      overflow: auto;
+      flex: 1;
+    }
+
+    #{$c}__actions {
+      padding-block: $popup-padding-top-bottom;
+      flex: 0 0 auto;
+    }
   }
 }
 
@@ -60,10 +125,15 @@ $popup-width-full-breakpoint: (
 @media (max-width: $popup-width-full-breakpoint) {
   .p-edit-profile {
     #{$c}__popup {
-      width: calc(
-        100% - #{2 * $popup-padding-sides} - #{$popup-width-full-margin-sides}
-      );
-      min-width: auto;
+      width: calc(100% - #{$popup-width-full-margin-sides});
+    }
+  }
+}
+
+@media (max-height: $popup-height-full-breakpoint) {
+  .p-edit-profile {
+    #{$c}__popup {
+      height: calc(100% - #{$popup-height-full-margin-top-bottom});
     }
   }
 }
