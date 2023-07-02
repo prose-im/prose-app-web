18aaa0596f3326fdadb774bbe2dad078a70cb3ae
Valerian Saliou <valerian@valeriansaliou.name>
2023-07-02 13:02:44 +0200

    feat: add base-spinner

---
+++ src/components/base/BaseSpinner.vue
@@ -0,0 +1,86 @@
+<!--
+ * This file is part of prose-app-web
+ *
+ * Copyright 2023, Prose Foundation
+ -->
+
+<!-- **********************************************************************
+     TEMPLATE
+     ********************************************************************** -->
+
+<template lang="pug">
+span.c-base-spinner
+  span(
+    :style="spinnerStyle"
+    class="c-base-spinner__clip"
+  )
+</template>
+
+<!-- **********************************************************************
+     SCRIPT
+     ********************************************************************** -->
+
+<script lang="ts">
+export default {
+  name: "BaseSpinner",
+
+  props: {
+    color: {
+      type: String,
+      default: "#000000"
+    },
+
+    size: {
+      type: String,
+      default: "20px"
+    },
+
+    borderWidth: {
+      type: String,
+      default: "2.5px"
+    },
+
+    speed: {
+      type: String,
+      default: "900ms"
+    }
+  },
+
+  computed: {
+    spinnerStyle(): object {
+      return {
+        height: this.size,
+        width: this.size,
+        borderColor: `${this.color} ${this.color} transparent`,
+        borderWidth: this.borderWidth,
+        animation: `rotate ${this.speed} 0s infinite linear`
+      };
+    }
+  }
+};
+</script>
+
+<!-- **********************************************************************
+     STYLE
+     ********************************************************************** -->
+
+<style lang="scss">
+$c: ".c-base-spinner";
+
+.c-base-spinner {
+  text-align: center;
+  line-height: 0;
+
+  &,
+  #{$c}__clip {
+    display: inline-block;
+  }
+
+  #{$c}__clip {
+    background-color: transparent;
+    border-style: solid;
+    border-radius: 100%;
+    animation-fill-mode: both;
+  }
+}
+</style>
