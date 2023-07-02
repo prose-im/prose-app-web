3e964a35588ad510760f40cadfa813e298094123
Valerian Saliou <valerian@valeriansaliou.name>
2023-07-02 13:57:42 +0200

    feat: bootstrap base-navigate component

---
+++ src/components/base/BaseNavigate.vue
@@ -0,0 +1,65 @@
+<!--
+ * This file is part of prose-app-web
+ *
+ * Copyright 2023, Prose Foundation
+ -->
+
+<!-- **********************************************************************
+     TEMPLATE
+     ********************************************************************** -->
+
+<template lang="pug">
+ul.c-base-navigate
+  li.c-base-navigate__section(
+    v-for="section in sections"
+  )
+    base-navigate-section(
+      :title="section.title"
+      :label="section.label"
+    )
+</template>
+
+<!-- **********************************************************************
+     SCRIPT
+     ********************************************************************** -->
+
+<script lang="ts">
+// INTERFACES
+export interface Section {
+  title: string;
+  label: string;
+}
+
+export default {
+  name: "BaseNavigate",
+
+  props: {
+    sections: {
+      type: Array,
+      required: true,
+
+      validator(x: Array<Section>): boolean {
+        return x.length > 0;
+      }
+    }
+  }
+};
+</script>
+
+<!-- **********************************************************************
+     STYLE
+     ********************************************************************** -->
+
+<style lang="scss">
+$c: ".c-base-navigate";
+
+.c-base-navigate {
+  #{$c}__section {
+    margin-block-end: 2px;
+
+    &:last-child {
+      margin-block-end: 0;
+    }
+  }
+}
+</style>
---
+++ src/components/base/BaseNavigateSection.vue
@@ -0,0 +1,77 @@
+<!--
+ * This file is part of prose-app-web
+ *
+ * Copyright 2023, Prose Foundation
+ -->
+
+<!-- **********************************************************************
+     TEMPLATE
+     ********************************************************************** -->
+
+<template lang="pug">
+a.c-base-navigate-section
+  span.c-base-navigate-section__icon
+
+  span.c-base-navigate-section__text
+    span.c-base-navigate-section__text-title.u-medium
+      | {{ title }}
+
+    span.c-base-navigate-section__text-label.u-medium
+      | {{ label }}
+
+  span.c-base-navigate-section__arrow
+</template>
+
+<!-- **********************************************************************
+     SCRIPT
+     ********************************************************************** -->
+
+<script lang="ts">
+export default {
+  name: "BaseNavigateSection",
+
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+
+    label: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+
+<!-- **********************************************************************
+     STYLE
+     ********************************************************************** -->
+
+<style lang="scss">
+$c: ".c-base-navigate-section";
+
+.c-base-navigate-section {
+  display: block;
+
+  #{$c}__icon {
+    /* TODO */
+  }
+
+  #{$c}__text {
+    /* TODO */
+
+    #{$c}__text-title {
+      /* TODO */
+    }
+
+    #{$c}__text-label {
+      /* TODO */
+    }
+  }
+
+  #{$c}__arrow {
+    /* TODO */
+  }
+}
+</style>
