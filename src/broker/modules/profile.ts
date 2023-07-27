85f4018d199b1550a5a08cd8f9738ef1dc6e6e2c
mb <mb@nesium.com>
2023-07-27 17:08:30 +0200

    chore: Remove unused code

--- src/broker/modules/profile.ts
+++ src/broker/modules/profile.ts
@@ -10,7 +10,7 @@
 
 // NPM
 import { Cash } from "cash-dom";
-import { $iq, Strophe } from "strophe.js";
+import { $iq } from "strophe.js";
 import xmppID from "@xmpp/id";
 import xmppTime from "@xmpp/time";
 import {
@@ -25,7 +25,7 @@ import {
 // PROJECT: BROKER
 import BrokerModule from "@/broker/modules";
 import { IQType } from "@/broker/stanzas/iq";
-import { NS_VCARD4, NS_LAST, NS_TIME } from "@/broker/stanzas/xmlns";
+import { NS_LAST, NS_TIME } from "@/broker/stanzas/xmlns";
 
 // PROJECT: UTILITIES
 import logger from "@/utilities/logger";
@@ -98,18 +98,6 @@ interface SaveAvatarRequestMetadata {
   height?: number;
   width?: number;
 }
-
-/**************************************************************************
- * INSTANCES
- * ************************************************************************* */
-
-const PHONE_URI_REGEX = /^tel:(.+)$/;
-
-/**************************************************************************
- * CONSTANTS
- * ************************************************************************* */
-
-const PHONE_URI_PREFIX = "tel:";
 
 /**************************************************************************
  * CLASS
@@ -227,82 +215,6 @@ class BrokerModuleProfile extends BrokerModule {
       tzo: timeElement.find("tzo").text() || xmppTime.offset(),
       utc: timeElement.find("utc").text() || xmppTime.datetime()
     };
-  }
-
-  private __makeStanzaVCard(
-    stanza: Strophe.Builder,
-    vCard: SaveVCardRequest
-  ): void {
-    const stanzaVCard = stanza.c("vcard", {
-      xmlns: NS_VCARD4
-    });
-
-    // Append full name
-    this.__makeStanzaVCardValue(stanzaVCard, "fn", vCard.fullName);
-
-    // Append name?
-    if (vCard.firstName || vCard.lastName) {
-      const stanzaVCardName = stanzaVCard.c("n");
-
-      if (vCard.firstName) {
-        stanzaVCardName.c("given", {}, vCard.firstName);
-      }
-      if (vCard.lastName) {
-        stanzaVCardName.c("surname", {}, vCard.lastName);
-      }
-
-      // Done, go back to root
-      stanzaVCardName.up();
-    }
-
-    // Append URL, email & phone
-    this.__makeStanzaVCardValue(stanzaVCard, "url", vCard.url, "uri");
-    this.__makeStanzaVCardValue(stanzaVCard, "email", vCard.email);
-
-    this.__makeStanzaVCardValue(
-      stanzaVCard,
-      "tel",
-      vCard.phone,
-      "uri",
-      PHONE_URI_PREFIX
-    );
-
-    // Append job?
-    if (vCard.job) {
-      this.__makeStanzaVCardValue(stanzaVCard, "title", vCard.job.title);
-      this.__makeStanzaVCardValue(stanzaVCard, "role", vCard.job.role);
-      this.__makeStanzaVCardValue(stanzaVCard, "org", vCard.job.organization);
-    }
-
-    // Append address?
-    if (vCard.address && (vCard.address.city || vCard.address.country)) {
-      const stanzaVCardAddress = stanzaVCard.c("adr");
-
-      if (vCard.address.city) {
-        stanzaVCardAddress.c("locality", {}, vCard.address.city);
-      }
-      if (vCard.address.country) {
-        stanzaVCardAddress.c("country", {}, vCard.address.country);
-      }
-
-      // Done, go back to root
-      stanzaVCardAddress.up();
-    }
-  }
-
-  private __makeStanzaVCardValue(
-    stanzaVCard: Strophe.Builder,
-    nodeName: string,
-    nodeValue: string | undefined,
-    wrapperType = "text",
-    valuePrefix = ""
-  ): void {
-    if (nodeValue) {
-      stanzaVCard
-        .c(nodeName)
-        .c(wrapperType, {}, `${valuePrefix}${nodeValue}`)
-        .up();
-    }
   }
 }
 
