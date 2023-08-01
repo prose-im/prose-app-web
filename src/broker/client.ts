61a802b1b7e935c5e2602f19696df46b1978831d
mb <mb@nesium.com>
2023-08-01 12:04:31 +0200

    chore: Remove StropheJSConnection and strophe.js which are now included in @prose-im/prose-core-client-wasm
    chore: Remove @types/strophe.js

--- src/broker/client.ts
+++ src/broker/client.ts
@@ -10,12 +10,8 @@
 
 // NPM
 import { default as $, Cash } from "cash-dom";
-import { Strophe } from "strophe.js";
 import {
   ProseClient,
-  ProseConnectionProvider,
-  ProseConnection,
-  ProseConnectionEventHandler,
   ProseClientDelegate,
   ConnectionError,
   Availability,
@@ -83,75 +79,6 @@ const RECONNECT_INTERVAL = 5000; // 5 seconds
  * CLASS
  * ************************************************************************* */
 
-class StropheJSConnectionProvider implements ProseConnectionProvider {
-  provideConnection(): ProseConnection {
-    return new StropheJSConnection();
-  }
-}
-
-class StropheJSConnection implements ProseConnection {
-  private readonly __connection: Strophe.Connection;
-  private __eventHandler?: ProseConnectionEventHandler;
-
-  constructor() {
-    this.__connection = new Strophe.Connection(
-      "wss://chat.prose.org/websocket/",
-      { protocol: "wss" }
-    );
-    this.__connection.rawInput = data => {
-      //console.log("RECV", data);
-      if (this.__eventHandler) {
-        this.__eventHandler.handleStanza(data);
-      }
-    };
-  }
-
-  async connect(jid: string, password: string) {
-    return new Promise<void>((resolve, reject) => {
-      this.__connection.connect(jid, password, status => {
-        if (status === Strophe.Status.CONNECTING) {
-          console.log("Strophe is connecting.");
-        } else if (status === Strophe.Status.CONNFAIL) {
-          console.log("Strophe failed to connect.");
-          reject(new Error("Something went wrong."));
-        } else if (status === Strophe.Status.DISCONNECTING) {
-          console.log("Strophe is disconnecting.");
-        } else if (status === Strophe.Status.DISCONNECTED) {
-          console.log("Strophe is disconnected.");
-        } else if (status === Strophe.Status.CONNECTED) {
-          console.log("Strophe is connected.");
-          resolve();
-
-          //connection.addHandler(onMessage, null, 'message', null, null,  null);
-          //connection.send($pres().tree());
-        }
-      });
-    });
-  }
-
-  disconnect() {
-    this.__connection.disconnect("logout");
-  }
-
-  sendStanza(stanza: string) {
-    console.log("Sending stanza", stanza);
-    const element = new DOMParser().parseFromString(
-      stanza,
-      "text/xml"
-    ).firstElementChild;
-
-    if (!element) {
-      throw new Error("Failed to parse stanza");
-    }
-
-    this.__connection.send(element);
-  }
-
-  setEventHandler(handler: ProseConnectionEventHandler) {
-    this.__eventHandler = handler;
-  }
-}
-
 class BrokerClient implements ProseClientDelegate {
   jid?: JID;
 
@@ -192,10 +119,7 @@ class BrokerClient implements ProseClientDelegate {
       password
     };
 
-    const client = await ProseClient.init(
-      new StropheJSConnectionProvider(),
-      this
-    );
+    const client = await ProseClient.init(this);
     this.client = client;
 
     try {
