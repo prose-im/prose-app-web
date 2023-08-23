e56c3f4b4a44b15ce091fdb02aff9a36e8486507
Valerian Saliou <valerian@valeriansaliou.name>
2023-08-23 20:06:51 +0200

    style: fix lint issue

--- src/modals/sidebar/AddContact.vue
+++ src/modals/sidebar/AddContact.vue
@@ -164,7 +164,7 @@ export default {
     },
 
     isVerified(): boolean {
-      return this.hasIdentity === true && this.identity.name;
+      return this.hasIdentity === true && this.identity.name ? true : false;
     },
 
     noticeIcon(): string {
