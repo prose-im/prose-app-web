af79b67151edf1be88ac4fc480491777abc2534d
Valerian Saliou <valerian@valeriansaliou.name>
2023-10-02 08:25:31 +0200

    feat: map all settings store setters

--- src/store/tables/settings.ts
+++ src/store/tables/settings.ts
@@ -34,6 +34,7 @@ const $settings = defineStore("settings", {
       notifications: {
         configuration: {
           topics: "all",
+          replies: true,
 
           when: {
             days: "weekdays",
@@ -103,6 +104,134 @@ const $settings = defineStore("settings", {
         }
       }
     };
+  },
+
+  actions: {
+    setAppearanceTheme(value: string): void {
+      this.setGeneric(this.appearance, "theme", value);
+    },
+
+    setAvailabilityAutoAwayEnabled(value: boolean): void {
+      this.setGeneric(this.availability.autoAway, "enabled", value);
+    },
+
+    setAvailabilityAutoAwayAfter(value: string): void {
+      this.setGeneric(this.availability.autoAway, "after", value);
+    },
+
+    setNotificationsConfigurationTopics(value: string): void {
+      this.setGeneric(this.notifications.configuration, "topics", value);
+    },
+
+    setNotificationsConfigurationReplies(value: boolean): void {
+      this.setGeneric(this.notifications.configuration, "replies", value);
+    },
+
+    setNotificationsConfigurationWhenDays(value: string): void {
+      this.setGeneric(this.notifications.configuration.when, "days", value);
+    },
+
+    setNotificationsConfigurationWhenTimeFrom(value: string): void {
+      this.setGeneric(
+        this.notifications.configuration.when.time,
+        "from",
+        value
+      );
+    },
+
+    setNotificationsConfigurationWhenTimeTo(value: string): void {
+      this.setGeneric(this.notifications.configuration.when.time, "to", value);
+    },
+
+    setNotificationsActionNotifyBadge(value: boolean): void {
+      this.setGeneric(this.notifications.action.notify, "badge", value);
+    },
+
+    setNotificationsActionNotifySound(value: boolean): void {
+      this.setGeneric(this.notifications.action.notify, "sound", value);
+    },
+
+    setNotificationsActionNotifyBanner(value: boolean): void {
+      this.setGeneric(this.notifications.action.notify, "banner", value);
+    },
+
+    setNotificationsDevicesMobileAlertsEnabled(value: boolean): void {
+      this.setGeneric(
+        this.notifications.devices.mobile.alerts,
+        "enabled",
+        value
+      );
+    },
+
+    setNotificationsDevicesMobileAlertsAfter(value: string): void {
+      this.setGeneric(this.notifications.devices.mobile.alerts, "after", value);
+    },
+
+    setMessagesChatsChatstates(value: boolean): void {
+      this.setGeneric(this.messages.chats, "chatstates", value);
+    },
+
+    setMessagesChatsSpellcheck(value: boolean): void {
+      this.setGeneric(this.messages.chats, "spellcheck", value);
+    },
+
+    setMessagesChatsClock24h(value: boolean): void {
+      this.setGeneric(this.messages.chats, "clock24h", value);
+    },
+
+    setMessagesFilesImagePreviews(value: boolean): void {
+      this.setGeneric(this.messages.files.imagePreviews, "enabled", value);
+    },
+
+    setMessagesFilesImageSize(value: string): void {
+      this.setGeneric(this.messages.files.imagePreviews, "size", value);
+    },
+
+    setCallsCameraInputSource(value: string): void {
+      this.setGeneric(this.calls.camera, "inputSource", value);
+    },
+
+    setCallsMicrophoneInputSource(value: string): void {
+      this.setGeneric(this.calls.microphone, "inputSource", value);
+    },
+
+    setCallSoundOutputSource(value: string): void {
+      this.setGeneric(this.calls.sound, "outputSource", value);
+    },
+
+    setUpdatesChannel(value: string): void {
+      this.setGeneric(this.updates, "channel", value);
+    },
+
+    setPrivacyReportAnalytics(value: boolean): void {
+      this.setGeneric(this.privacy.report, "analytics", value);
+    },
+
+    setPrivacyReportCrashes(value: boolean): void {
+      this.setGeneric(this.privacy.report, "crashes", value);
+    },
+
+    setGeneric<ValueType>(
+      container: { [key: string]: ValueType },
+      key: string,
+      value: ValueType
+    ): boolean {
+      // Check if will change
+      const willChange = container[key] !== value ? true : false;
+
+      if (willChange === true) {
+        // Update value
+        this.$patch(() => {
+          container[key] = value;
+        });
+
+        // Has changed
+        return true;
+      }
+
+      // Has not changed
+      return false;
+    }
   }
 });
 
