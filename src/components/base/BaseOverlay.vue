10c48c927f3bd39de7985b897b641ea8876fc8be
Valerian Saliou <valerian@valeriansaliou.name>
2023-10-22 19:00:01 +0200

    feat: base-overlay component

---
+++ src/components/base/BaseOverlay.vue
@@ -0,0 +1,40 @@
+<!--
+ * This file is part of prose-app-web
+ *
+ * Copyright 2023, Prose Foundation
+ -->
+
+<!-- **********************************************************************
+     TEMPLATE
+     ********************************************************************** -->
+
+<template lang="pug">
+.c-base-overlay
+  slot
+</template>
+
+<!-- **********************************************************************
+     SCRIPT
+     ********************************************************************** -->
+
+<script lang="ts">
+export default {
+  name: "BaseOverlay"
+};
+</script>
+
+<!-- **********************************************************************
+     STYLE
+     ********************************************************************** -->
+
+<style lang="scss">
+$c: ".c-base-overlay";
+
+.c-base-overlay {
+  background-color: rgba(var(--color-white), 0.65);
+  backdrop-filter: blur(6px);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>
