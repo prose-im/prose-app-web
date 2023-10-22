d7e06a712ee111eb7899c9b17e78075f0c1bbfd6
Valerian Saliou <valerian@valeriansaliou.name>
2023-10-22 15:13:26 +0200

    feat: inbox-form-attach component

---
+++ src/components/inbox/InboxFormAttach.vue
@@ -0,0 +1,72 @@
+<!--
+ * This file is part of prose-app-web
+ *
+ * Copyright 2023, Prose Foundation
+ -->
+
+<!-- **********************************************************************
+     TEMPLATE
+     ********************************************************************** -->
+
+<template lang="pug">
+.c-inbox-form-attach
+  input.c-inbox-form-attach__input(
+    @change.prevent="onInputChange"
+    type="file",
+    name="attach",
+    title="",
+    tabindex="-1",
+    multiple
+  )
+</template>
+
+<!-- **********************************************************************
+     SCRIPT
+     ********************************************************************** -->
+
+<script lang="ts">
+export default {
+  name: "InboxFormAttach",
+
+  emits: ["file"],
+
+  methods: {
+    // --> EVENT LISTENERS <--
+
+    onInputChange(event: InputEvent): void {
+      const target = (event.target as HTMLInputElement) || null;
+
+      if (target?.files && target?.files.length > 0) {
+        for (const file of target.files) {
+          this.$emit("file", file);
+        }
+      }
+    }
+  }
+};
+</script>
+
+<!-- **********************************************************************
+     STYLE
+     ********************************************************************** -->
+
+<style lang="scss">
+$c: ".c-inbox-form-attach";
+
+.c-inbox-form-attach {
+  position: absolute;
+  inset: 0;
+  z-index: 1;
+  overflow: hidden;
+
+  #{$c}__input {
+    cursor: pointer;
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    padding: 0;
+    display: block;
+    opacity: 0;
+  }
+}
+</style>
