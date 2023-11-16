3ec716bacf5c7ade2faed2e551fb52262150b5b9
Valerian Saliou <valerian@valeriansaliou.name>
2023-11-16 11:45:09 +0400

    feat: add ability to include an icon in base-count

--- src/components/base/BaseCount.vue
+++ src/components/base/BaseCount.vue
@@ -16,7 +16,17 @@ div(
     "c-base-count--" + color
   ]`
 )
-  | {{ count }}
+  base-icon(
+    v-if="icon"
+    :name="icon"
+    size="12px"
+    class="c-base-count__icon"
+  )
+
+  template(
+    v-else
+  )
+    | {{ count }}
 </template>
 
 <!-- **********************************************************************
@@ -31,6 +41,11 @@ export default {
     count: {
       type: Number,
       default: 0
+    },
+
+    icon: {
+      type: String,
+      default: null
     },
 
     color: {
@@ -64,16 +79,28 @@ $count-padding-sides: 5px;
   min-width: ($count-size-base - (2 * $count-padding-sides));
   border-radius: 4px;
 
+  #{$c}__icon {
+    margin-block-end: -1.5px;
+  }
+
   // --> COLORS <--
 
   &--blue {
     background-color: rgb(var(--color-base-blue-normal));
     color: rgb(var(--color-white));
+
+    #{$c}__icon {
+      fill: rgb(var(--color-white));
+    }
   }
 
   &--white {
     background-color: rgb(var(--color-white));
     color: rgb(var(--color-black));
+
+    #{$c}__icon {
+      fill: rgb(var(--color-black));
+    }
   }
 }
 </style>
