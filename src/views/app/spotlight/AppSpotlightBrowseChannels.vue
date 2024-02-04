b207940bc6be5901d7370c0272aee93d84127a31
Valerian Saliou <valerian@valeriansaliou.name>
2024-02-04 18:45:57 -0500

    feat: finish browse channels view

--- src/views/app/spotlight/AppSpotlightBrowseChannels.vue
+++ src/views/app/spotlight/AppSpotlightBrowseChannels.vue
@@ -29,8 +29,21 @@ list-browse(
      ********************************************************************** -->
 
 <script lang="ts">
+// NPM
+import { JID, RoomID } from "@prose-im/prose-sdk-js";
+
 // PROJECT: COMPONENTS
+import BaseAlert from "@/components/base/BaseAlert.vue";
+import BaseIcon from "@/components/base/BaseIcon.vue";
+import BaseButton from "@/components/base/BaseButton.vue";
 import { Groups as ListBrowseGroups } from "@/components/list/ListBrowse.vue";
+
+// PROJECT: BROKER
+import Broker from "@/broker";
+
+// PROJECT: STORES
+import Store from "@/store";
+import { ChannelEntry, ChannelType } from "@/store/tables/channel";
 
 export default {
   name: "AppSpotlightBrowseChannels",
@@ -39,14 +52,150 @@ export default {
     return {
       // --> STATE <--
 
-      loading: false
+      loading: false,
+
+      pendingJoins: {} as { [roomId: RoomID]: boolean }
     };
   },
 
   computed: {
+    channelListPublic(): ReturnType<typeof Store.$channel.getList> {
+      return Store.$channel.getList().filter((entry: ChannelEntry) => {
+        return entry.type === ChannelType.Public;
+      });
+    },
+
     groups(): ListBrowseGroups {
-      // TODO
+      if (this.channelListPublic.length > 0) {
+        return [
+          {
+            title: {
+              name: "Public channels",
+              aside: `${this.channelListPublic.length} available`
+            },
+
+            results: this.channelListPublic.map((entry: ChannelEntry) => {
+              const entryRoomId = entry.jid as RoomID,
+                entryRoomJID = new JID(entry.jid),
+                entryRoomMaybe = Store.$room.getRoom(entryRoomId),
+                entryLoading = this.pendingJoins[entryRoomId] || false,
+                isRoomJoined = entryRoomMaybe !== undefined;
+
+              return {
+                icon: {
+                  component: BaseIcon,
+
+                  properties: {
+                    name: "circle.grid.2x2",
+                    size: "12px",
+                    fill: "rgb(var(--color-base-blue-dark)"
+                  }
+                },
+
+                identity: {
+                  primary: entry.name
+                },
+
+                actions: [
+                  {
+                    component: BaseButton,
+                    label: isRoomJoined === false ? "Join Channel" : "Open",
+
+                    properties: {
+                      tint: "dark",
+                      size: "medium",
+                      reverse: isRoomJoined,
+                      disabled: entryLoading,
+                      loading: entryLoading
+                    },
+
+                    listeners: {
+                      click: async () => {
+                        if (isRoomJoined === true) {
+                          await this.onBrowseActionNavigate(entryRoomId);
+                        } else {
+                          await this.onBrowseActionJoin(
+                            entryRoomJID,
+                            entryRoomId
+                          );
+                        }
+                      }
+                    }
+                  }
+                ]
+              };
+            })
+          }
+        ];
+      }
+
       return [];
+    }
+  },
+
+  mounted() {
+    // Ensure that data is loaded
+    this.ensureLoaded();
+  },
+
+  methods: {
+    // --> HELPERS <--
+
+    async ensureLoaded(): Promise<void> {
+      this.loading = true;
+
+      try {
+        // Ensure that channel list is loaded
+        // Notice: forcibly refresh from server.
+        await Store.$channel.load(true);
+      } catch (error) {
+        this.$log.error("Could not load channel list", error);
+
+        // Show error alert
+        BaseAlert.error(
+          "Error loading",
+          "Channels could not be loaded. Try again?"
+        );
+      } finally {
+        this.loading = false;
+      }
+    },
+
+    // --> EVENT LISTENERS <--
+
+    async onBrowseActionJoin(roomJID: JID, roomId: RoomID): Promise<void> {
+      if (this.pendingJoins[roomId] !== true) {
+        this.pendingJoins[roomId] = true;
+
+        try {
+          // Join channel
+          await Broker.$room.join(roomJID);
+
+          // Show information alert
+          BaseAlert.success("Channel joined", "Public channel has been joined");
+        } catch (error) {
+          this.$log.error("Could not join public channel", error);
+
+          // Show error alert
+          BaseAlert.error(
+            "Cannot join channel",
+            "Could not join this public channel. Try this again?"
+          );
+        } finally {
+          delete this.pendingJoins[roomId];
+        }
+      }
+    },
+
+    async onBrowseActionNavigate(roomId: RoomID): Promise<void> {
+      // Navigate to channel
+      this.$router.push({
+        name: "app.inbox",
+
+        params: {
+          roomId
+        }
+      });
     }
   }
 };
