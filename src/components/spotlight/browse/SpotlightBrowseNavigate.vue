75f8cdef9bc31101a67d6e769038f31778254802
Valerian Saliou <valerian@valeriansaliou.name>
2024-02-04 15:12:32 -0500

    feat: populate spotlight-browse-navigate

--- src/components/spotlight/browse/SpotlightBrowseNavigate.vue
+++ src/components/spotlight/browse/SpotlightBrowseNavigate.vue
@@ -10,6 +10,40 @@
 
 <template lang="pug">
 .c-spotlight-browse-navigate
+  list-button(
+    v-for="section in sections"
+    @click="onSectionClick(section)"
+    :active="routeName === section.route"
+    :class=`[
+      "c-spotlight-browse-navigate__section",
+      {
+        "c-spotlight-browse-navigate__section--active": (routeName === section.route)
+      }
+    ]`
+  )
+    template(
+      v-slot:icon
+    )
+      base-icon(
+        :name="section.icon"
+        size="16px"
+        class="c-spotlight-browse-navigate__section-icon"
+      )
+
+    template(
+      v-slot:default
+    )
+      | {{ section.title }}
+
+    template(
+      v-if="section.count"
+      v-slot:details
+    )
+      base-count(
+        v-if="section.count"
+        :count="section.count"
+        :color="(routeName === section.route) ? 'white' : 'red'"
+      )
 </template>
 
 <!-- **********************************************************************
@@ -17,8 +51,68 @@
      ********************************************************************** -->
 
 <script lang="ts">
+// PROJECT: COMPONENTS
+import SidebarMainSection from "@/components/sidebar/SidebarMainSection.vue";
+
+// INTERFACES
+interface Section {
+  route: string;
+  title: string;
+  icon: string;
+  count?: number;
+}
+
 export default {
-  name: "SpotlightBrowseNavigate"
+  name: "SpotlightBrowseNavigate",
+
+  components: {
+    SidebarMainSection
+  },
+
+  computed: {
+    routeName(): string {
+      return typeof this.$route.name === "string" ? this.$route.name : "";
+    },
+
+    sections(): Array<Section> {
+      return [
+        {
+          route: "app.spotlight.browse.people",
+          title: "People",
+          icon: "person.crop.circle"
+        },
+
+        {
+          route: "app.spotlight.browse.channels",
+          title: "Channels",
+          icon: "rectangle.stack"
+        },
+
+        {
+          route: "app.spotlight.browse.invites",
+          title: "Invites",
+          icon: "bell.fill",
+          count: 1
+        },
+
+        {
+          route: "app.spotlight.browse.blocked",
+          title: "Blocked",
+          icon: "minus.circle.fill"
+        }
+      ];
+    }
+  },
+
+  methods: {
+    // --> EVENT LISTENERS <--
+
+    onSectionClick(section: Section): void {
+      this.$router.push({
+        name: section.route
+      });
+    }
+  }
 };
 </script>
 
@@ -30,6 +124,20 @@ export default {
 $c: ".c-spotlight-browse-navigate";
 
 .c-spotlight-browse-navigate {
-  /* TODO */
+  padding-inline: 4px;
+  padding-block: 14px;
+  box-sizing: border-box;
+
+  #{$c}__section {
+    #{$c}__section-icon {
+      fill: rgb(var(--color-base-grey-dark));
+    }
+
+    &--active {
+      #{$c}__section-icon {
+        fill: rgb(var(--color-white));
+      }
+    }
+  }
 }
 </style>
