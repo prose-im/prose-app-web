2280f1cee68af9b0e6ae02da7fb4c10be7b24c0b
Valerian Saliou <valerian@valeriansaliou.name>
2024-02-04 11:21:03 -0500

    feat: add empty spotlight-browse-topbar

---
+++ src/assemblies/spotlight/SpotlightBrowseTopbar.vue
@@ -0,0 +1,37 @@
+<!--
+ * This file is part of prose-app-web
+ *
+ * Copyright 2024, Prose Foundation
+ -->
+
+<!-- **********************************************************************
+     TEMPLATE
+     ********************************************************************** -->
+
+<template lang="pug">
+layout-toolbar(
+  class="a-spotlight-browse-topbar"
+)
+</template>
+
+<!-- **********************************************************************
+     SCRIPT
+     ********************************************************************** -->
+
+<script lang="ts">
+export default {
+  name: "SpotlightBrowseTopbar"
+};
+</script>
+
+<!-- **********************************************************************
+     STYLE
+     ********************************************************************** -->
+
+<style lang="scss">
+$c: ".a-spotlight-browse-topbar";
+
+.a-spotlight-browse-topbar {
+  /* TODO */
+}
+</style>
