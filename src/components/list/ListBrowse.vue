c76eeff1bbffbc6bf9c94d1389c6f994548a37ca
Valerian Saliou <valerian@valeriansaliou.name>
2024-02-04 17:46:36 -0500

    feat: add list-browse and list-browse-result list components

---
+++ src/components/list/ListBrowse.vue
@@ -0,0 +1,173 @@
+<!--
+ * This file is part of prose-app-web
+ *
+ * Copyright 2024, Prose Foundation
+ -->
+
+<!-- **********************************************************************
+     TEMPLATE
+     ********************************************************************** -->
+
+<template lang="pug">
+.c-list-browse
+  .c-list-browse__loading(
+    v-if="loading"
+  )
+    base-spinner(
+      size="14px"
+      border-width="1.5px"
+    )
+
+  .c-list-browse__empty(
+    v-else-if="groups.length === 0"
+  )
+    base-placeholder-image(
+      :illustration="emptyIllustration"
+      :title="emptyTitle"
+      :description="emptyDescription"
+    )
+
+  template(
+    v-else
+  )
+    .c-list-browse__group(
+      v-for="group in groups"
+    )
+      .c-list-browse__title(
+        v-if="group.title"
+      )
+        span.c-list-browse__title-name.u-bold
+          | {{ group.title.name }}
+
+        span.c-list-browse__title-aside(
+          v-if="group.title.aside"
+        )
+          | {{ group.title.aside }}
+
+      ul.c-list-browse__results
+        list-browse-result(
+          v-for="result in group.results"
+          :result="result"
+          class="c-list-browse__result"
+        )
+</template>
+
+<!-- **********************************************************************
+     SCRIPT
+     ********************************************************************** -->
+
+<script lang="ts">
+// NPM
+import { PropType } from "vue";
+
+// PROJECT: COMPONENTS
+import { Result as ListBrowseResult } from "@/components/list/ListBrowseResult.vue";
+
+// TYPES
+export type Groups = Array<Group>;
+
+// ENUMERATIONS
+export interface Group {
+  title?: {
+    name: string;
+    aside?: string;
+  };
+
+  results: Array<ListBrowseResult>;
+}
+
+export default {
+  name: "ListBrowse",
+
+  props: {
+    groups: {
+      type: Array as PropType<Groups>,
+      required: true
+    },
+
+    emptyIllustration: {
+      type: String,
+      required: true
+    },
+
+    emptyTitle: {
+      type: String,
+      default: "No results found."
+    },
+
+    emptyDescription: {
+      type: String,
+      default: null
+    },
+
+    loading: {
+      type: Boolean,
+      default: false
+    }
+  }
+};
+</script>
+
+<!-- **********************************************************************
+     STYLE
+     ********************************************************************** -->
+
+<style lang="scss">
+$c: ".c-list-browse";
+
+.c-list-browse {
+  #{$c}__loading,
+  #{$c}__empty {
+    height: 100%;
+    display: flex;
+    justify-content: center;
+  }
+
+  #{$c}__loading {
+    padding-block-start: 5px;
+  }
+
+  #{$c}__empty {
+    align-items: center;
+  }
+
+  #{$c}__group {
+    margin-block-end: 34px;
+
+    &:last-child {
+      margin-block-end: 0;
+    }
+
+    #{$c}__title {
+      margin-block-end: 14px;
+      padding-inline: 6px;
+      display: flex;
+      align-items: center;
+
+      #{$c}__title-name {
+        color: rgb(var(--color-text-primary));
+        font-size: 15.5px;
+        letter-spacing: 0.1px;
+        flex: 1;
+      }
+
+      #{$c}__title-aside {
+        color: rgb(var(--color-text-secondary));
+        font-size: 13.5px;
+        margin-inline-start: 8px;
+        flex: 0 0 auto;
+      }
+    }
+
+    #{$c}__results {
+      #{$c}__result {
+        margin-block-end: 6px;
+
+        &:last-child {
+          margin-block-end: 0;
+        }
+      }
+    }
+  }
+}
+</style>
---
+++ src/components/list/ListBrowseResult.vue
@@ -0,0 +1,154 @@
+<!--
+ * This file is part of prose-app-web
+ *
+ * Copyright 2024, Prose Foundation
+ -->
+
+<!-- **********************************************************************
+     TEMPLATE
+     ********************************************************************** -->
+
+<template lang="pug">
+li.c-list-browse-result
+  .c-list-browse-result__icon(
+    v-if="result.icon"
+  )
+    component(
+      v-bind="result.icon.properties"
+      :is="result.icon.component"
+    )
+
+  .c-list-browse-result__main
+    .c-list-browse-result__identity.u-ellipsis
+      span.c-list-browse-result__identity-primary.u-bold
+        | {{ result.identity.primary }}
+
+      span.c-list-browse-result__identity-secondary(
+        v-if="result.identity.secondary"
+      )
+        | {{ result.identity.secondary }}
+
+    .c-list-browse-result__preview.u-ellipsis(
+      v-if="result.preview"
+    )
+      | {{ result.preview }}
+
+  .c-list-browse-result__actions(
+    v-if="result.actions && result.actions.length > 0"
+  )
+    component(
+      v-for="action in result.actions"
+      v-bind="action.properties"
+      :is="action.component"
+    )
+      | {{ action.label }}
+</template>
+
+<!-- **********************************************************************
+     SCRIPT
+     ********************************************************************** -->
+
+<script lang="ts">
+// NPM
+import { PropType } from "vue";
+
+// ENUMERATIONS
+export interface Result {
+  icon?: {
+    component: object;
+    properties?: object;
+  };
+
+  identity: {
+    primary: string;
+    secondary?: string;
+  };
+
+  preview?: string;
+
+  actions?: [
+    {
+      component: object;
+      label: string;
+      properties?: object;
+    }
+  ];
+}
+
+export default {
+  name: "ListBrowseResult",
+
+  props: {
+    result: {
+      type: Object as PropType<Result>,
+      required: true
+    }
+  }
+};
+</script>
+
+<!-- **********************************************************************
+     STYLE
+     ********************************************************************** -->
+
+<style lang="scss">
+$c: ".c-list-browse-result";
+
+.c-list-browse-result {
+  background-color: rgb(var(--color-white));
+  border: 1px solid rgb(var(--color-border-primary));
+  padding-inline: 18px;
+  padding-block: 10px;
+  display: flex;
+  align-items: center;
+  overflow: hidden;
+  border-radius: 3px;
+  box-shadow: 0 2px 6px 0 rgba(var(--color-shadow-primary), 0.02);
+
+  #{$c}__icon {
+    margin-inline-end: 16px;
+    flex: 0 0 auto;
+  }
+
+  #{$c}__main {
+    overflow: hidden;
+    flex: 1;
+
+    #{$c}__identity {
+      line-height: 16px;
+      margin-block-start: -1px;
+
+      #{$c}__identity-primary {
+        color: rgb(var(--color-text-primary));
+        font-size: 14px;
+      }
+
+      #{$c}__identity-secondary {
+        color: rgb(var(--color-text-secondary));
+        font-size: 13px;
+        margin-inline-start: 10px;
+      }
+    }
+
+    #{$c}__preview {
+      color: rgb(var(--color-text-secondary));
+      font-size: 12.5px;
+      line-height: 14px;
+      margin-block-start: 4px;
+    }
+  }
+
+  #{$c}__actions {
+    margin-inline-start: 14px;
+    flex: 0 0 auto;
+
+    > * {
+      margin-inline-end: 5px;
+
+      &:last-child {
+        margin-inline-end: 0;
+      }
+    }
+  }
+}
+</style>
