d8fa5a24460357a58a3c7ca83283ebe1d77aabf1
Valerian Saliou <valerian@valeriansaliou.name>
2024-02-10 13:01:55 -0500

    feat: add data broker module

---
+++ src/broker/modules/data.ts
@@ -0,0 +1,42 @@
+/*
+ * This file is part of prose-app-web
+ *
+ * Copyright 2024, Prose Foundation
+ */
+
+/**************************************************************************
+ * IMPORTS
+ * ************************************************************************* */
+
+// NPM
+import { UploadSlot } from "@prose-im/prose-sdk-js";
+
+// PROJECT: BROKER
+import BrokerModule from "@/broker/modules";
+
+// PROJECT: UTILITIES
+import logger from "@/utilities/logger";
+
+/**************************************************************************
+ * CLASS
+ * ************************************************************************* */
+
+class BrokerModuleData extends BrokerModule {
+  async requestUploadSlot(
+    fileName: string,
+    fileSize: number
+  ): Promise<UploadSlot | void> {
+    logger.info(`Will request upload slot for file name: '${fileName}'`);
+
+    return await this._client.client?.requestUploadSlot(
+      fileName,
+      BigInt(fileSize)
+    );
+  }
+}
+
+/**************************************************************************
+ * EXPORTS
+ * ************************************************************************* */
+
+export default BrokerModuleData;
