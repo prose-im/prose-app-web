f001b16f76b73153b457e070d1d7dfe702483e61
Valerian Saliou <valerian@valeriansaliou.name>
2024-03-10 12:38:58 +0100

    chore: cleanup tauri setup

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,3 +1,7 @@
+// This file is part of prose-app-web
+//
+// Copyright 2023, Prose Foundation
+
 fn main() {
     tauri_build::build()
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,12 @@
+// This file is part of prose-app-web
+//
+// Copyright 2023, Prose Foundation
+
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
-
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
