df4dea7e8b817f0c608f93b23a7e907a052f3626
Valerian Saliou <valerian@valeriansaliou.name>
2024-05-11 21:58:23 +0200

    refactor(popups): split encryption-settings

---
+++ src/components/popups/inbox/AccountSettingsIdentities.vue
@@ -0,0 +1,79 @@
+<!--
+ * This file is part of prose-app-web
+ *
+ * Copyright 2024, Prose Foundation
+ -->
+
+<!-- **********************************************************************
+     TEMPLATE
+     ********************************************************************** -->
+
+<template lang="pug">
+.p-encryption-settings-identities
+  ul.p-encryption-settings-identities__devices
+    li.p-encryption-settings-identities__device(
+      v-for="device in devices"
+      :key="'device_' + device.fingerprint"
+    )
+      .p-encryption-settings-identities__device-details
+        span.p-encryption-settings-identities__device-emoji
+          | {{ device.emoji }}
+
+        span.p-encryption-settings-identities__device-fingerprint
+          | {{ device.fingerprint }}
+
+        span.p-encryption-settings-identities__device-name
+          | {{ device.name }}
+
+      .p-encryption-settings-identities__device-options
+</template>
+
+<!-- **********************************************************************
+     SCRIPT
+     ********************************************************************** -->
+
+<script lang="ts">
+export default {
+  name: "AccountSettingsIdentities",
+
+  data() {
+    return {
+      // --> DATA <--
+
+      devices: [
+        {
+          name: "MacBook Valerian",
+          emoji: "🦹🚶‍♂️🍖🥪🧀",
+
+          fingerprint: [
+            "45a37f26",
+            "5d7da857",
+            "07848791",
+            "6e1e10e6",
+            "23debc0e",
+            "8d173327",
+            "2e4185a3",
+            "d3d6bd3a"
+          ].join(" ")
+        },
+
+        {
+          name: "iPhone Valerian",
+          emoji: "🦹🚶‍♂️🍖🥪🧀",
+
+          fingerprint: [
+            "15a37f26",
+            "5d7da857",
+            "07848791",
+            "3e1e10e6",
+            "23debc0e",
+            "8d173327",
+            "1e4185a3",
+            "d3d6bd3a"
+          ].join(" ")
+        }
+      ]
+    };
+  }
+};
+</script>
--- src/popups/inbox/EncryptionSettings.vue
+++ src/popups/inbox/EncryptionSettings.vue
@@ -11,14 +11,26 @@
 <template lang="pug">
 layout-popup-navigate(
   @close="onClose"
+  :content-section="section"
   size="small"
   class="p-encryption-settings"
 )
   template(
+    v-slot:navigate
+  )
+    base-navigate(
+      @navigate="onSectionsNavigate"
+      :sections="navigateSections"
+      :active-id="section"
+    )
+
+  template(
     v-slot:content
   )
-    form-settings-editor(
-      :fieldsets="fieldsets"
+    component(
+      v-if="contentSections[section]"
+      v-bind="contentSections[section].properties"
+      :is="contentSections[section].component"
     )
 
   template(
@@ -36,110 +48,69 @@ layout-popup-navigate(
      ********************************************************************** -->
 
 <script lang="ts">
+// NPM
+import { shallowRef } from "vue";
+
 // PROJECT: COMPONENTS
-import {
-  default as FormSettingsEditor,
-  Fieldset as FormFieldset,
-  FieldsetFieldType as FormFieldsetFieldType,
-  FieldsetFieldDataToggle as FormFieldsetFieldDataToggle
-} from "@/components/form/FormSettingsEditor.vue";
+import { Section as NavigateSection } from "@/components/base/BaseNavigate.vue";
+import AccountSettingsIdentities from "@/components/popups/inbox/AccountSettingsIdentities.vue";
+import AccountSettingsPreferences from "@/components/popups/inbox/AccountSettingsPreferences.vue";
 
 export default {
   name: "EncryptionSettings",
 
-  components: { FormSettingsEditor },
+  components: { AccountSettingsIdentities, AccountSettingsPreferences },
+
+  props: {
+    sectionInitial: {
+      type: String,
+      default: "identities"
+    }
+  },
 
   emits: ["close"],
 
   data() {
     return {
+      // --> STATE <--
+
+      section: this.sectionInitial,
+
       // --> DATA <--
 
-      fieldsets: [
+      navigateSections: [
         {
-          id: "local",
-          title: "End-to-end encryption (on what I send)",
-
-          fields: [
-            {
-              id: "messages",
-              type: FormFieldsetFieldType.Toggle,
-              label: "Sent messages:",
-
-              data: {
-                value: {
-                  inner: true // TODO: save somewhere
-                },
-
-                disabled: true
-              } as FormFieldsetFieldDataToggle
-            },
-
-            {
-              id: "files",
-              type: FormFieldsetFieldType.Toggle,
-              label: "Shared files:",
-
-              data: {
-                value: {
-                  inner: true // TODO: save somewhere
-                },
-
-                disabled: true
-              } as FormFieldsetFieldDataToggle
-            },
-
-            {
-              id: "calls",
-              type: FormFieldsetFieldType.Toggle,
-              label: "Video calls:",
-
-              data: {
-                value: {
-                  inner: true // TODO: save somewhere
-                },
-
-                disabled: true
-              } as FormFieldsetFieldDataToggle
-            }
-          ],
-
-          notes: [
-            "End-to-end encryption is enabled by default on all private communications with all users. It is heavily recommended to leave all options enabled.",
-            "Using end-to-end encryption guarantees that no one other than you and the intended recipient can access the content you send (eg. messages, files and calls). Not even the server administrator.",
-            "If you are experiencing issues sending content to the other user, you can disable some or all encryption. Doing so will not disable encryption between this app and your server."
-          ]
+          id: "identities",
+          title: "Identities",
+          icon: "checkmark.seal.fill"
         },
 
         {
-          id: "remote",
-          title: "End-to-end encryption (on what I receive)",
-
-          fields: [
-            {
-              id: "request",
-              type: FormFieldsetFieldType.Toggle,
-              label: "Please encrypt:",
-
-              data: {
-                value: {
-                  inner: true // TODO: save somewhere
-                },
-
-                disabled: true
-              } as FormFieldsetFieldDataToggle
-            }
-          ],
-
-          notes: [
-            "You can ask the other party not to encrypt content they send to you, even if they support end-to-end encryption. Leave this enabled, unless you are experiencing issues eg. receiving messages."
-          ]
+          id: "preferences",
+          title: "Preferences",
+          icon: "gearshape"
         }
-      ] as Array<FormFieldset>
+      ] as Array<NavigateSection>,
+
+      contentSections: {
+        identities: {
+          component: shallowRef(AccountSettingsIdentities)
+        },
+
+        preferences: {
+          component: shallowRef(AccountSettingsPreferences)
+        }
+      }
     };
   },
 
   methods: {
+    // --> HELPERS <--
+
+    onSectionsNavigate(sectionId: string): void {
+      this.section = sectionId;
+    },
+
     // --> EVENT LISTENERS <--
 
     onClose(): void {
