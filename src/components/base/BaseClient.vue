8761281683d87517999e861ceb5b99dc93f66841
Valerian Saliou <valerian@valeriansaliou.name>
2024-07-26 11:45:42 +0200

    feat: add base-client

---
+++ src/components/base/BaseClient.vue
@@ -0,0 +1,133 @@
+<!--
+ * This file is part of prose-app-web
+ *
+ * Copyright 2024, Prose Foundation
+ -->
+
+<!-- **********************************************************************
+     TEMPLATE
+     ********************************************************************** -->
+
+<template lang="pug">
+span.c-base-client
+  span(
+    v-if="clientImageUrl"
+    :style="logoStyle"
+    class="c-base-client__logo"
+  )
+
+  span(
+    :style="nameStyle"
+    class="c-base-client__name u-medium"
+  )
+    | {{ name }}
+</template>
+
+<!-- **********************************************************************
+     SCRIPT
+     ********************************************************************** -->
+
+<script lang="ts">
+// CONSTANTS
+const LOGO_MARGIN_INLINE_END_FROM_SIZE_RATIO = 0.25;
+const NAME_FONT_SIZE_OFFSET = 2;
+
+const CLIENT_IMAGE_NAMES = {
+  Adium: "adium",
+  Aparté: "aparte",
+  AstraChat: "astrachat",
+  ChatSecure: "chatsecure",
+  Conversations: "conversations",
+  Dino: "dino",
+  Gajim: "gajim",
+  Jappix: "jappix",
+  Monal: "monal",
+  Movim: "movim",
+  Moxxy: "moxxy",
+  Pidgin: "pidgin",
+  Poezio: "poezio",
+  Prose: "prose",
+  Psi: "psi",
+  "Psi+": "psi",
+  Siskin: "siskin",
+  Snikket: "snikket",
+  Yaxim: "yaxim"
+};
+
+export default {
+  name: "BaseClient",
+
+  props: {
+    name: {
+      type: String,
+      required: true
+    },
+
+    size: {
+      type: String,
+      default: "16px"
+    }
+  },
+
+  computed: {
+    clientImageUrl(): string | null {
+      const clientImageName = CLIENT_IMAGE_NAMES[this.name] || null;
+
+      if (clientImageName !== null) {
+        return `/images/components/base/BaseClient/${clientImageName}.png`;
+      }
+
+      return null;
+    },
+
+    sizeNumeric(): number {
+      return parseInt(this.size.replace("px", ""));
+    },
+
+    logoStyle(): { [property: string]: string | null } {
+      return {
+        backgroundImage:
+          this.clientImageUrl !== null ? `url("${this.clientImageUrl}")` : null,
+
+        height: this.size,
+        width: this.size,
+
+        marginInlineEnd: `${Math.floor(
+          this.sizeNumeric * LOGO_MARGIN_INLINE_END_FROM_SIZE_RATIO
+        )}px`
+      };
+    },
+
+    nameStyle(): { [property: string]: string } {
+      return {
+        fontSize: `${this.sizeNumeric - NAME_FONT_SIZE_OFFSET}px`,
+        lineHeight: this.size
+      };
+    }
+  }
+};
+</script>
+
+<!-- **********************************************************************
+     STYLE
+     ********************************************************************** -->
+
+<style lang="scss">
+$c: ".c-base-client";
+
+#{$c} {
+  display: inline-flex;
+  align-items: center;
+
+  #{$c}__logo {
+    background-position: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+    flex: 0 0 auto;
+  }
+
+  #{$c}__name {
+    color: rgb(var(--color-text-primary));
+  }
+}
+</style>
