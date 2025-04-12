3f88d7579caf01d2e402f9b02e35314b3c1cb054
Valerian Saliou <valerian@valeriansaliou.name>
2025-04-12 17:35:36 +0200

    fix: make tauri commands work again

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -3,5 +3,26 @@
 // Copyright 2024, Prose Foundation
 
 fn main() {
-    tauri_build::build()
+    tauri_build::try_build(
+        tauri_build::Attributes::new()
+            .plugin(
+                "connection",
+                tauri_build::InlinedPlugin::new().commands(&[
+                    "connect",
+                    "disconnect",
+                    "destroy",
+                    "send",
+                ]),
+            )
+            .plugin(
+                "download",
+                tauri_build::InlinedPlugin::new().commands(&["download_file"]),
+            )
+            .plugin(
+                "notification",
+                tauri_build::InlinedPlugin::new()
+                    .commands(&["send_notification", "set_badge_count"]),
+            ),
+    )
+    .expect("failed to run tauri-build");
 }
